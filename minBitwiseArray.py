26842169b7dd3e7f69118966272e0750a54959a0
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-20 12:57:27 -0500

    added comment

--- minBitwiseArray.py
+++ minBitwiseArray.py
@@ -11,12 +11,12 @@ class Solution:
             # Count trailing 1s
             s = 0
             temp = p
-            while temp & 1:
+            while temp & 1: # While the least significant bit is 1
                 s += 1
-                temp >>= 1
+                temp >>= 1 # Shift temp right by 1
 
             # Minimum x
-            x = p - (1 << (s - 1))
+            x = p - (1 << (s - 1)) # p - 2^(s-1)
             ans.append(x)
 
         return ans
