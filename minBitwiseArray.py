3a91ec68cb30a111a3434d5ad08132659adc0668
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-19 21:38:21 -0500

    Solution found

--- minBitwiseArray.py
+++ minBitwiseArray.py
@@ -1,3 +1,22 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        pass
+        ans = []
+
+        for p in nums:
+            # If p is even, impossible
+            if p % 2 == 0:
+                ans.append(-1)
+                continue
+
+            # Count trailing 1s
+            s = 0
+            temp = p
+            while temp & 1:
+                s += 1
+                temp >>= 1
+
+            # Minimum x
+            x = p - (1 << (s - 1))
+            ans.append(x)
+
+        return ans
