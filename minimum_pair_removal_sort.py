07e4df65323e5bb960515f45929ae46a2381279a
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-21 22:19:23 -0500

    solution added

--- minimum_pair_removal_sort.py
+++ minimum_pair_removal_sort.py
@@ -1,3 +1,20 @@
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        pass
+        def non_decreasing(nums):
+            return all(nums[i] <= nums[i+1] for i in range(len(nums) - 1))
+
+        count = 0
+        while not non_decreasing(nums) and len(nums) != 1:
+            min_sum = float("inf")
+            idx = 0
+            for i in range(len(nums) - 1):
+                s = nums[i] + nums[i + 1]
+                if s < min_sum:
+                    min_sum = s
+                    idx = i
+            
+            # replace the pair with their sum
+            nums = nums[:idx] + [min_sum] + nums[idx + 2:]
+            count += 1
+
+        return count
