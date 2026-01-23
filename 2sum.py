8ee47658aec3b162182efd8c62f99d00b7d4ab4a
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-23 14:44:55 -0500

    added

--- 2sum.py
+++ 2sum.py
@@ -1,3 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        n = len(nums)
+        indexed = list(enumerate(nums))
+        indexed.sort(key=lambda x: x[1])
+        index = 0
+        for i in range(n-1):
+            if indexed[i][1] + indexed[i+1][1] == target:
+                index = i
+                break
+
+        return [indexed[i][0], indexed[i+1][0]]
