a134005f9c193ffdfc66c9bcbb0c31529d3f2620
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-24 13:40:46 -0500

    Good JOB, JZL!

--- Minimize_max_pair_sum.py
+++ Minimize_max_pair_sum.py
@@ -1,3 +1,13 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
-        pass
+        nums.sort()
+        max_sum = float('-inf')
+        i, r = 0, len(nums)-1
+        while i < r:
+            s = nums[i] + nums[r]
+            if s > max_sum:
+                max_sum = s
+            i += 1
+            r -= 1
+
+        return max_sum
