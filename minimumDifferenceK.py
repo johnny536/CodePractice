001caff4e8a194e39d83bf0db30f2cc81c82497f
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-25 16:46:49 -0500

    Greate Job, JinZiLu!

--- minimumDifferenceK.py
+++ minimumDifferenceK.py
@@ -1,3 +1,18 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
-        pass
+        n = len(nums)
+        
+        if n == 1:
+            return 0
+
+        nums.sort()
+        if k == n:
+            return nums[n-1] - nums[0]
+
+        max_diff = float('inf')
+        for i in range(n-k+1):
+            s = nums[i+k-1] - nums[i]
+            if s < max_diff:
+                max_diff = s
+
+        return max_diff
