17f5d81c6932ae709ff0db4370529364c6fd0281
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-25 22:01:28 -0500

    Exceleent JohnyJJJJJJ

--- minAbsDiff.py
+++ minAbsDiff.py
@@ -1,3 +1,16 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        pass
+        arr.sort()
+        res = []
+
+        min = float('inf')
+        for i in range(len(arr)-1):
+            s = arr[i+1] - arr[i]
+            if s == min:
+                res.append([arr[i], arr[i+1]])
+            elif s < min:
+                res.clear()
+                min = s
+                res.append([arr[i], arr[i+1]])
+
+        return res
