b6eed9ad861d5c106c8ddc1fc3b82da163d997d3
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-01-31 01:39:18 -0500

    Good job! Efficent Solution, JZL!

--- nextSmallestLetterGreater.py
+++ nextSmallestLetterGreater.py
@@ -1,3 +1,26 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        pass
+        letters.sort()
+        
+        s = 0
+
+        left, right = 0, len(letters) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if letters[mid] == target:
+                s = mid
+                break
+            elif letters[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        while s < len(letters) and letters[s] <= target:
+            s += 1
+
+        if s < len(letters) and letters[s] > target:
+            return letters[s]
+        else:
+            return letters[0]
