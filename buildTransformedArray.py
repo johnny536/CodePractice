4ab2fb82feea67d3a0855fb3c95d5d00e1fa7238
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-05 16:13:46 -0500

    Question Solved!, transformedArray!

--- buildTransformedArray.py
+++ buildTransformedArray.py
@@ -1,3 +1,16 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        pass
+        result = []
+        n = len(nums)
+        for i in range(n):
+            value = nums[i]
+            step = i+value
+            if step >= n:
+                while step >= n:
+                    step -= n
+            elif step < -n:
+                while step < -n:
+                    step += n
+            #print(step)
+            result.append(nums[step]) #cannot use result[i]
+        return result
