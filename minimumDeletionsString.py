8c575c73f46fedac6b0e295328e525e2e6393ea5
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-06 21:52:20 -0500

    Correct Now!

--- minimumDeletionsString.py
+++ minimumDeletionsString.py
@@ -1,3 +1,26 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        pass
+        idx_a = s.find('a')
+        idx_firstb = s.find('b')
+        if idx_firstb == -1 or idx_a == -1:
+            return 0
+
+        minDel = float('inf')
+        n = len(s)
+
+        # prefix_b[i] = number of 'b' in s[0:i]
+        prefix_b = [0] * (n + 1)
+        for i in range(n):
+            prefix_b[i + 1] = prefix_b[i] + (s[i] == 'b')
+
+        # suffix_a[i] = number of 'a' in s[i:n]
+        suffix_a = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix_a[i] = suffix_a[i + 1] + (s[i] == 'a')
+
+        for i in range(idx_firstb, n+1):
+            cost = prefix_b[i] + suffix_a[i]
+            if cost < minDel:
+                minDel = cost
+
+        return minDel
