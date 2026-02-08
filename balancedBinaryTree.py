89a8c93697e2699cd1e2b3f750001100d75aec74
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-07 20:20:49 -0500

    solution added

--- balancedBinaryTree.py
+++ balancedBinaryTree.py
@@ -7,20 +7,27 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def height(node: Optional[TreeNode]) -> int:
+            
+            #An empty tree has height 0 and is balanced
             if not node:
                 return 0
 
+            #Recursively get left height, if left subtree is not balanced, return -1
             lh = height(node.left)
             if lh == -1:
                 return -1
 
+            #Recursively get right height, if right subtree is not balanced, return -1
             rh = height(node.right)
             if rh == -1:
                 return -1
 
+            #If the difference in height is more than 1, it's not balanced, return -1
             if abs(lh - rh) > 1:
                 return -1
 
+            #If balanced, return height of current node which is 1 + max of left and right heights
+            result = 0
             return 1 + max(lh, rh)
 
         return height(root) != -1
