481b35be75202e89e1c0bfad99ca43345c4dd82d
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-08 23:07:01 -0500

    excellent solution!

--- balanceBST.py
+++ balanceBST.py
@@ -6,4 +6,26 @@
 #         self.right = right
 class Solution:
     def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        # 1) Inorder traversal -> sorted values (BST property)
+        vals = []
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            vals.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+
+        # 2) Build balanced BST from sorted array
+        def build(l, r):
+            if l > r:
+                return None
+            m = (l + r) // 2
+            node = TreeNode(vals[m])
+            node.left = build(l, m - 1)
+            node.right = build(m + 1, r)
+            return node
+
+        return build(0, len(vals) - 1)
