2362742f18e4b9c7579ec7815307aad4095d1f0f
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-09 20:21:55 -0500

    correct!

--- longestBalancedSub.py
+++ longestBalancedSub.py
@@ -1,5 +1,24 @@
 class Solution:
-    
+    # correct solution
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+
+        seen = [0] * (max(nums) + 1)
+
+        for i in range(n):
+            A = [0, 0]
+
+            for j in range(i, n):
+                val = nums[j]
+                if seen[val] != i + 1:
+                    seen[val] = i + 1
+                    A[val & 1] += 1
+
+                if A[0] == A[1]:
+                    res = max(res, j - i + 1)
+
+        return res
     
     # This is partially correct, not working for subarray not ending at the end of the array
     def wrongLongestBalanced(self, nums: List[int]) -> int:
