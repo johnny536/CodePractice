22d28c45139e9524169198e7923307f1584c958c
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-12 18:52:06 -0500

    good one!

--- Longest_Balanced_Char_I.py
+++ Longest_Balanced_Char_I.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestBalanced(self, s: str) -> int:
-        pass
+        n = len(s)
+        res = 0
+
+        for i in range(n):
+            counts = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+
+            for j in range(i, n):
+                counts[s[j]] += 1
+                if len(set([v for v in counts.values() if v != 0])) == 1:
+                    res = max(res, j-i+1)
+
+        return res
