d1f3953b42ecea185cbd5708668e639678b47065
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-18 18:47:41 -0500

    Great Job!

--- BinaryNumberWithAlterBits.py
+++ BinaryNumberWithAlterBits.py
@@ -1,3 +1,11 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        pass
+        #Remember this
+        binary_rep = format(n,'b')
+        prev = '1'
+        for i in range(1, len(binary_rep)):
+            if binary_rep[i] == prev:
+                return False
+            prev = binary_rep[i]
+        #True is uppercase
+        return True
