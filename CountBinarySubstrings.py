e249a0e0bed716fea9f7e129fa2f5f775a40e64d
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-19 01:43:21 -0500

    solution solution lennnn

--- CountBinarySubstrings.py
+++ CountBinarySubstrings.py
@@ -1,3 +1,23 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        pass
+        accumulate_len = []
+        prev = s[0]
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == prev:
+                count += 1
+            else:
+                prev = s[i]
+                accumulate_len.append(count)
+                count = 1
+        #forgot this, this is necessary!!!
+        accumulate_len.append(count)
+        #used for debugging
+        #print(accumulate_len)
+
+        res = 0
+        for i in range(1, len(accumulate_len)):
+            smaller = min(accumulate_len[i-1], accumulate_len[i])
+            res += smaller
+        
+        return res
