2977b98236b2347fa416d5bf58785819730a8088
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-21 02:53:15 -0500

    EZ

--- CountPrimeSetBits.py
+++ CountPrimeSetBits.py
@@ -1,3 +1,10 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        pass
+        res = 0
+        for x in range(left, right+1):
+            binary_rep = format(x, 'b')
+            count = binary_rep.count('1')
+            if count in [2, 3, 5, 7, 11, 13, 17,19]:
+                res += 1
+
+        return res
