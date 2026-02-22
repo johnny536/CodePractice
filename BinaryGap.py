ecab61218e1cc5c8db821796881ca96e82f71afe
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-02-22 00:27:54 -0500

    EZ

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,3 +1,15 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        pass
+        binary_rep = format(n, 'b')
+        count = 1
+        accumulated_Zero = []
+        for i in range(1, len(binary_rep)):
+            if binary_rep[i] == '0':
+                count += 1
+            else:
+                accumulated_Zero.append(count)
+                count = 1
+        if not accumulated_Zero:
+            return 0
+        else:
+            return max(accumulated_Zero)
