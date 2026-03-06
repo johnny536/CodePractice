cfcb01d65ace93a51169551a5ef1aee7a1e0affe
Zilu(Johnny) Jin <johnnyjin758@gmail.com>
2026-03-06 17:44:00 -0500

    EZ

--- CheckBSOnesSegment.py
+++ CheckBSOnesSegment.py
@@ -1,3 +1,11 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        pass
+        firstZero = False
+        result = True   
+        for i in range(1, len(s)):
+            if s[i] == '0' and not firstZero:
+                firstZero = True
+            if s[i] == '1' and firstZero and result:
+                return False
+        
+        return result
